46bffcda5e485fbb66ab29897190bf1a739463f1
Jatinrajput123 <rajputjatin815@gmail.com>
2026-03-31 07:38:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
+import connectCloudinary from './config/cloudinary.js';
+const app=express();
+const PORT=8080;
+connectDB().then(()=>{
+    console.log("DB connected successfully")
+ }).catch((err)=>{
+    console.log("DB not connected")
+ })
+
+ connectCloudinary();
+// middleware
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+
+app.use('/api/user',userRouter);
+app.use('/api/product',productRouter)
+
+
+app.get('/',(req,res)=>{
+res.send("api work")
+})
+
+app.listen(PORT,()=>{
+    console.log(`listening on port no ${PORT}`)
+});
